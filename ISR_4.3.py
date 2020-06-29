5df5ab3052fca36b9a207ea10db391a078662091
Plyaskina Ulyana <31893521+Akwatore@users.noreply.github.com>
2020-06-29 11:12:09 +0300

    Update ISR_4.3.py

--- ISR_4.3.py
+++ ISR_4.3.py
@@ -3,29 +3,29 @@ import random
 from PIL import Image, ImageDraw
 
 def text_to_qr(url, filename, *, scale=500, quality=100):
-    result = pyqrcode.create(url).text().splitlines()
+    qr = pyqrcode.create(url).text().splitlines()
 
-    width = len(result[0])
-    height = len(result)
+    width = len(qr[0])
+    height = len(qr)
 
     im = Image.new(mode='RGB', size=(width, height))
     draw = ImageDraw.Draw(im)
 
-    colors = [127, 255]
+    colors = [150, 255]
 
     for y in range(height):
         for x in range(width):
-            dot = int(result[y][x])
-            color = tuple([dot * random.randint(128, 255) for i in range(3)])
+            dot = int(qr[y][x])
+            color = tuple([dot * random.randint(151, 255) for i in range(3)])
             draw.point(
                 xy=(x, y),
                 fill=color
                 )
     im = im.resize((scale, scale))
-    im.save('result.jpg', quality=quality)
+    im.save('qr.jpg', quality=quality)
 
 
 text_to_qr(
-    'haha pyqrcode go brrr | pls dont yoink the code i wrote it myself @3am',
-    filename='result.jpg'
+    'hi',
+    filename='qr.jpg'
     )
