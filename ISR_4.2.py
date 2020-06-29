3d770ee5cfd6b7f1b5f08e128f691626c8be1db1
Plyaskina Ulyana <31893521+Akwatore@users.noreply.github.com>
2020-06-29 11:17:01 +0300

    Update ISR_4.2.py

--- ISR_4.2.py
+++ ISR_4.2.py
@@ -2,10 +2,10 @@ import pyqrcode
 from PIL import Image, ImageDraw
 
 def text_to_qr(url, filename, *, scale=500, quality=100):
-    result = pyqrcode.create(url).text().splitlines()
+    qr = pyqrcode.create(url).text().splitlines()
 
-    width = len(result[0])
-    height = len(result)
+    width = len(qr[0])
+    height = len(qr)
 
     im = Image.new(mode='RGB', size=(width, height))
     draw = ImageDraw.Draw(im)
@@ -14,13 +14,17 @@ def text_to_qr(url, filename, *, scale=500, quality=100):
         for x in range(width):
             draw.point(
                 xy=(x, y),
-                fill=tuple([int(result[y][x]) * 255 for i in range(3)])
+                fill=tuple([int(qr[y][x]) * 255 for i in range(3)])
                 )
     im = im.resize((scale, scale))
-    im.save('result.jpg', quality=quality)
+    im.save('qr.jpg', quality=quality)
 
 
 text_to_qr(
-    'haha pyqrcode go brrr | pls dont yoink the code i wrote it myself @3am',
-    filename='result.jpg'
+    'hi',
+    filename='qr.jpg'
     )
+
+
+#Ссылка на repl
+#https://repl.it/@ulyaakwatore/Siem-5-Tiema-4-ISR-42#main.py
